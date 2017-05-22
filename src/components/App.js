9c6b936b792089c569938310a761104c147680ec
Stéphane Steffffff <Stphane Steffffff>
2017-05-22 22:36:05 +0200

    add App.js

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class App extends React.Component{
+
+  render(){
+    return(
+      <div>
+        <h1> Lorem Ipsum </h1>
+      </div>
+    )
+  }
+
+}
+export default App;
---
+++ src/index.js
@@ -0,0 +1,13 @@
+// React import
+import React from 'react';
+import { render } from 'react-dom';
+
+// Components
+import App from './components/App';
+
+
+
+render (
+  <App />,
+  document.getElementById('root')
+);
