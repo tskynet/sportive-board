3551d7d2c6d540ee0622fb3d35d7058b85c5139f
Stéphane Steffffff <Stphane Steffffff>
2017-06-04 23:52:56 +0200

    add somes css

--- src/components/Listing.js
+++ src/components/Listing.js
@@ -5,9 +5,9 @@ class Listing extends React.Component{
 
   render(){
     return(
-      <div>
+      <div className="listing">
         <ul>
-          <li><b>{this.props.seance[1]}</b> {this.props.seance[0]} <i>Duration : {this.props.seance[2]} min !</i></li>
+          <li><b>{this.props.seance[1]}</b> {this.props.seance[0]} <i>Duration : <b>{this.props.seance[2]}</b> min !</i></li>
  
 
         </ul>
